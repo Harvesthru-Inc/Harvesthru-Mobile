1187bb701d219e4f75a83c79c79c7ad2735cdbe5
hwanggit <luw055@ucsd.edu>
2019-11-23 15:17:04 -0800

    set up server

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+app.use(cors());
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+
+// Mongo Atlas custom URL to mongoose connection
+const URL = process.env.URL;
+
+mongoose
+  .connect(URL, {
+    dbName: 'authentication',
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Now connected to MongoDB!'))
+  .catch(err => console.error('Something went wrong', err));
+
+app.get('/', (req, res) => {
+  res.send("Connected!")
+});
+
+app.listen(9000, () => {
+  console.log('Server listening on Port 9000!');
+});
