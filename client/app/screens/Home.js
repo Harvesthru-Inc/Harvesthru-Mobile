666a53e51935ea90a78f8fbb1912b335e9c87937
hwanggit <luw055@ucsd.edu>
2020-04-22 19:44:04 -0700

    fixed maps

--- client/app/screens/Home.js
+++ client/app/screens/Home.js
@@ -1,22 +1,60 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
-import NextButton from '../components/NextButton';
+import {View, Dimensions, Animated} from 'react-native';
 import MapView from 'react-native-maps';
+import HomeStyles from 'app/assets/stylesheets/HomeStyles';
+import {getStatusBarHeight} from 'react-native-status-bar-height';
 
 export default class Home extends React.Component {
+  constructor(props) {
+    this.previousHeight = Animated.Value(MIN_SCROLL_HEIGHT);
+    this.currentHeight = Animated.Value(MIN_SCROLL_HEIGHT);
+    this.heightOffset = Animated.Value(0);
+
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderGrant: event => {
+        this.heightOffset.setValue(0);
+      },
+      onPanResponderMove: (event, gesture) => {
+        const initialY = gesture.y0;
+        const newY = gesture.moveY;
+        const offset = -(newY - initialY) * SMOOTH_FACTOR;
+
+        this.heightOffset.setValue(offset);
+        const newHeight = this.previousHeight._value + offset;
+
+        if (newHeight >= MAX_SCROLL_HEIGHT) {
+          this.currentHeight.setValue(MAX_SCROLL_HEIGHT);
+        } else if (newHeight <= DEFAULT_SCROLL_HEIGHT) {
+          this.currentHeight.setValue(DEFAULT_SCROLL_HEIGHT);
+        } else {
+          this.currentHeight.setValue(newHeight);
+        }
+      },
+      onPanResponderRelease: (event, gesture) => {
+        const temp = this.previousHeight._value + this.heightOffset._value;
+        const MAX_SCROLL_HEIGHT = Dimensions.get('window').height;
+
+        if (temp >= MAX_SCROLL_HEIGHT) {
+          this.previousHeight.setValue(MAX_SCROLL_HEIGHT);
+        } else if (temp <= DEFAULT_SCROLL_HEIGHT) {
+          this.previousHeight.setValue(DEFAULT_SCROLL_HEIGHT);
+        } else {
+          this.previousHeight.setValue(temp);
+        }
+      },
+    });
+  }
+
   render() {
     return (
-      <SafeAreaView style={styles.container}>
-        <MapView />
-      </SafeAreaView>
+      <View>
+        <MapView style={HomeStyles.container} />
+      </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+const MIN_SCROLL_HEIGHT = 40;
+const MAX_SCROLL_HEIGHT =
+  Dimensions.get('window').height - getStatusBarHeight();
