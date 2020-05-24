6c6eebd9ee270aafb07acf52435498684a5edb12
hwanggit <luw055@ucsd.edu>
2020-05-23 20:36:54 -0700

    moved to root folder

--- client/ios/Pods/Target Support Files/Pods-Harvesthru-tvOS/Pods-Harvesthru-tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Harvesthru_tvOS : NSObject
-@end
-@implementation PodsDummy_Pods_Harvesthru_tvOS
-@end
--- client/ios/Pods/Target Support Files/Pods-Harvesthru/Pods-Harvesthru-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Harvesthru : NSObject
-@end
-@implementation PodsDummy_Pods_Harvesthru
-@end
